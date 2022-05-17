eee73e95ba6cc39a2a6416ed34737799c46b15ca
David Morgan <30870056+haitheredavid@users.noreply.github.com>
2022-05-17 12:55:53 -0400

    modified speckle objects

---
+++ Objects/ViewObjectsSpeckle/ViewStudyBase.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace ViewObjects.Speckle
+{
+
+  public class ViewStudyBase : ViewObjBase
+  {
+
+    public ViewStudyBase()
+    { }
+    public string viewName { get; set; }
+
+    public List<ViewObjBase> objs { get; set; }
+  }
+}
